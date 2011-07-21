8affb7b78a3699a4c343019a68103abaf47e4fb8
Justin Riley <justin.t.riley@gmail.com>
2011-07-20 22:22:55 -0400

    moved core code into single GHWikiPreview class
    this fixes an issue with detailed error messages not being reported due
    to not writing to global variables properly (globals()['key'] = value).
    could have been fixed as is, however, using globals() is verbose and
    error prone so moving to a class is a logical choice anyway :P
    
    added an error message for the case of no internet connection

--- plugin/ghwiki.py
+++ plugin/ghwiki.py
@@ -10,7 +10,6 @@ import BaseHTTPServer as httpserv
 
 from github import github
 
-REPO_ERROR = ''
 
 FORMATS_AND_EXTENSIONS = {'.asciidoc': 'asciidoc',
                           '.creole': 'creole',
@@ -26,58 +25,13 @@ FORMATS_AND_EXTENSIONS.update(
     dict.fromkeys(['.markdown', '.mdown', '.mkdn', '.mkd', '.md'], 'markdown'))
 
 
-def get_preview_url():
-    wiki_repo_defined = int(vim.eval("exists('g:ghwiki_preview_repo')"))
-    if not wiki_repo_defined:
-        REPO_ERROR = "please set ghwiki_preview_repo in your ~/.vimrc"
-        return
-    wiki_repo = vim.eval("g:ghwiki_preview_repo")
-    if len(wiki_repo.split('/')) != 2:
-        REPO_ERROR = "invalid ghwiki_preview_repo set, "
-        REPO_ERROR += "must have the form: 'user/repo'"
-        return
-    user, repo = wiki_repo.split('/')
-    gh = github.GitHub()
-    try:
-        repo_exists = gh.repos.show(user, repo)
-        if not repo_exists.has_wiki:
-            REPO_ERROR = "repo %s does not have a git-backed wiki enabled"
-            REPO_ERROR = REPO_ERROR % repo
-            return
-    except urllib2.HTTPError:
-        REPO_ERROR = "repo %s does not exist" % wiki_repo
-        return
-    return 'https://github.com/%s/%s/wiki/_preview' % (user, repo)
-
-URL = get_preview_url()
-
-
-def _build_and_quote_params(paramdict):
-    params = []
-    for key in paramdict:
-        params.append('wiki[%s]=%s' % (key, paramdict.get(key)))
-    return urllib2.quote('&'.join(params), safe='=&')
-
-
-def ghwiki_preview_buffer():
-    if not URL and REPO_ERROR:
-        print "!!! ERROR (ghwiki-preview): %s" % REPO_ERROR
-        return
-    buf = vim.current.buffer
-    bufname = os.path.basename(buf.name)
-    bufext = os.path.splitext(buf.name)[-1]
-    bufformat = FORMATS_AND_EXTENSIONS.get(bufext)
-    if bufformat is None:
-        print "ghwiki-preview: unsupported file extension '%s'" % bufext
-        return
-    params = dict(name=bufname, format=bufformat, body='\n'.join(buf))
-    params_quoted = _build_and_quote_params(params)
-    html = urllib2.urlopen(URL, data=params_quoted).read()
-    show_preview(html)
-
-
 class BackgroundBrowser(webbrowser.GenericBrowser):
-    """Class for all browsers which are to be started in the background."""
+    """
+    Class for all browsers which are to be started in the background.
+
+    (same as BackgroundBrowser included in webbrowser module except pipe stdout
+    in order to keep the browser's output from cluttering the vim buffer)
+    """
     def open(self, url, new=0, autoraise=1):
         cmdline = [self.name] + [arg.replace("%s", url)
                                  for arg in self.args]
@@ -95,51 +49,112 @@ class BackgroundBrowser(webbrowser.GenericBrowser):
             return False
 
 
-def _is_exe(fpath):
-    return os.path.exists(fpath) and os.access(fpath, os.X_OK)
+class GHWikiPreview(object):
 
+    _err = ''
+    _url = None
 
-def _which(program):
-    fpath, fname = os.path.split(program)
-    if fpath:
-        if _is_exe(program):
-            return program
-    else:
-        for path in os.environ["PATH"].split(os.pathsep):
-            exe_file = os.path.join(path, program)
-            if _is_exe(exe_file):
-                return exe_file
+    @property
+    def url(self):
+        if not self._url:
+            self._url = self._get_preview_url()
+        return self._url
 
+    def _get_preview_url(self):
+        wiki_repo_defined = int(vim.eval("exists('g:ghwiki_preview_repo')"))
+        if not wiki_repo_defined:
+            self._err = "please set ghwiki_preview_repo in your ~/.vimrc"
+            return
+        wiki_repo = vim.eval("g:ghwiki_preview_repo")
+        if len(wiki_repo.split('/')) != 2:
+            self._err = "invalid ghwiki_preview_repo set, "
+            self._err += "must have the form: 'user/repo'"
+            return
+        user, repo = wiki_repo.split('/')
+        gh = github.GitHub()
+        try:
+            repo_exists = gh.repos.show(user, repo)
+            if not repo_exists.has_wiki:
+                self._err = "repo %s does not have a git-backed wiki enabled"
+                self._err = self._err % repo
+                return
+        except urllib2.HTTPError:
+            self._err = "repo %s does not exist" % wiki_repo
+            return
+        except urllib2.URLError:
+            self._err = "no internet connection available"
+            return
+        return 'https://github.com/%s/%s/wiki/_preview' % (user, repo)
 
-def open_browser(url):
-    # get 'default' browser from webbrowser module
-    browser_cmd = webbrowser.get().name
-    browser_defined = int(vim.eval("exists('g:ghwiki_preview_browser')"))
-    if browser_defined:
-        browser_cmd = vim.eval("g:ghwiki_preview_browser")
-    cmd = shlex.split(browser_cmd)
-    arg0 = cmd[0]
-    if not _which(arg0):
-        raise Exception("browser %s does not exist" % arg0)
-    if "%s" not in browser_cmd:
-        cmd.append("%s")
-    browser = BackgroundBrowser(cmd)
-    browser.open(url)
+    def _build_and_quote_params(self, paramdict):
+        params = []
+        for key in paramdict:
+            params.append('wiki[%s]=%s' % (key, paramdict.get(key)))
+        return urllib2.quote('&'.join(params), safe='=&')
 
+    def ghwiki_preview_buffer(self):
+        if not self.url:
+            errmsg = self._err or 'error occured while fetching preview'
+            print "!!! ERROR (ghwiki-preview): %s" % errmsg
+            return
+        buf = vim.current.buffer
+        bufname = os.path.basename(buf.name)
+        bufext = os.path.splitext(buf.name)[-1]
+        bufformat = FORMATS_AND_EXTENSIONS.get(bufext)
+        if bufformat is None:
+            print "ghwiki-preview: unsupported file extension '%s'" % bufext
+            return
+        params = dict(name=bufname, format=bufformat, body='\n'.join(buf))
+        params_quoted = self._build_and_quote_params(params)
+        html = urllib2.urlopen(self.url, data=params_quoted).read()
+        self.show_preview(html)
 
-def show_preview(html):
-    class RequestHandler(httpserv.BaseHTTPRequestHandler):
-        def do_GET(self):
-            bufferSize = 1024 * 1024
-            for i in xrange(0, len(html), bufferSize):
-                self.wfile.write(html[i:i + bufferSize])
-    # create a temporary http server to handle one request
-    server = httpserv.HTTPServer(('127.0.0.1', 0), RequestHandler)
-    # calls URL to retrieve html from the temporary http server
-    previewurl = 'http://127.0.0.1:%s' % server.server_port
-    print "Local one-time preview url: %s " % previewurl
-    try:
-        open_browser(previewurl)
-        server.handle_request()
-    except Exception, e:
-        print e
+    def _is_exe(self, fpath):
+        return os.path.exists(fpath) and os.access(fpath, os.X_OK)
+
+    def _which(self, program):
+        fpath, fname = os.path.split(program)
+        if fpath:
+            if self._is_exe(program):
+                return program
+        else:
+            for path in os.environ["PATH"].split(os.pathsep):
+                exe_file = os.path.join(path, program)
+                if self._is_exe(exe_file):
+                    return exe_file
+
+    def open_browser(self, url):
+        # get 'default' browser from webbrowser module
+        browser_cmd = webbrowser.get().name
+        browser_defined = int(vim.eval("exists('g:ghwiki_preview_browser')"))
+        if browser_defined:
+            browser_cmd = vim.eval("g:ghwiki_preview_browser")
+        cmd = shlex.split(browser_cmd)
+        arg0 = cmd[0]
+        if not self._which(arg0):
+            raise Exception("browser %s does not exist" % arg0)
+        if "%s" not in browser_cmd:
+            cmd.append("%s")
+        browser = BackgroundBrowser(cmd)
+        browser.open(url)
+
+    def show_preview(self, html):
+        class RequestHandler(httpserv.BaseHTTPRequestHandler):
+            def do_GET(self):
+                bufferSize = 1024 * 1024
+                for i in xrange(0, len(html), bufferSize):
+                    self.wfile.write(html[i:i + bufferSize])
+        # create a temporary http server to handle one request
+        server = httpserv.HTTPServer(('127.0.0.1', 0), RequestHandler)
+        # calls URL to retrieve html from the temporary http server
+        previewurl = 'http://127.0.0.1:%s' % server.server_port
+        print "Local one-time preview url: %s " % previewurl
+        try:
+            self.open_browser(previewurl)
+            server.handle_request()
+        except Exception, e:
+            print e
+
+_obj = GHWikiPreview()
+
+ghwiki_preview_buffer = _obj.ghwiki_preview_buffer
